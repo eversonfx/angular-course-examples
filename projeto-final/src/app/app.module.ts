e55ad526fb6d83c38b65a1576d8c129e9dc88b10
eversonfx <eversonfelix_mb@hotmail.com>
2021-07-29 13:59:41 -0300

    Projeto dashboard e final

---
+++ projeto-final/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardModule } from './dashboard';
+import { CalculadoraModule } from './calculadora';
+import { ConversorModule } from './conversor';
+import { FormsModule } from '@angular/forms';
+import { TarefasModule } from './tarefas';
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DashboardModule,
+    CalculadoraModule,
+    ConversorModule,
+    TarefasModule,
+    JogoDaVelhaModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
